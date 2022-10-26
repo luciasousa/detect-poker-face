dc8ce9532279d606b98834c9c70540158e44edeb
luciasousa <luciamsousa00@ua.pt>
2022-10-26 01:28:08 +0100

    efficientNetB0 and DenseNet169 for FER2013

---
+++ article18/denseNet169_FER.py
@@ -0,0 +1,162 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sn
+from tensorflow import keras
+from keras.preprocessing.image import ImageDataGenerator
+from keras.applications import EfficientNetB0
+from keras.layers import Dense, Flatten, Dropout,BatchNormalization ,Activation, Conv2D, MaxPooling2D, GlobalAveragePooling2D, AveragePooling2D  
+from keras.models import Model, Sequential
+from keras.applications.nasnet import NASNetLarge
+from keras.callbacks import ReduceLROnPlateau, ModelCheckpoint, EarlyStopping
+from keras.optimizers import Adam
+from keras.preprocessing import image
+from keras.utils import plot_model
+from IPython.display import Image
+from keras.metrics import BinaryAccuracy, Precision, Recall, AUC
+#import deeplake
+
+#ds_train = deeplake.load('hub://activeloop/fer2013-train')
+#ds_test = deeplake.load('hub://activeloop/fer2013-test')
+#ds_priv_test = deeplake.load('hub://activeloop/fer2013-private-test')
+
+train_datagen = ImageDataGenerator( rescale = 1./255,
+                                    validation_split = 0.2,                         
+                                    rotation_range=5,
+                                    width_shift_range=0.2,
+                                    height_shift_range=0.2,
+                                    shear_range=0.2,
+                                    #zoom_range=0.2,
+                                    horizontal_flip=True,
+                                    vertical_flip=True,
+                                    fill_mode='nearest')
+
+valid_datagen = ImageDataGenerator(rescale = 1./255,validation_split = 0.2)
+
+test_datagen  = ImageDataGenerator(rescale = 1./255)
+
+train_dataset  = train_datagen.flow_from_directory(directory = '../../input/fer2013/train',
+                                                   target_size = (48,48),
+                                                   class_mode = 'categorical',
+                                                   subset = 'training',
+                                                   batch_size = 64)
+                                    
+valid_dataset = valid_datagen.flow_from_directory(directory = '../../input/fer2013/train',
+                                                  target_size = (48,48),
+                                                  class_mode = 'categorical',
+                                                  subset = 'validation',
+                                                  batch_size = 64)
+                                    
+test_dataset = test_datagen.flow_from_directory(directory = '../../input/fer2013/test',
+                                                  target_size = (48,48),
+                                                  class_mode = 'categorical',
+                                                  batch_size = 64)                            
+
+base_model = EfficientNetB0(input_shape=(48,48,3),include_top=False,weights="imagenet")
+
+for layer in base_model.layers[:-4]:
+    layer.trainable=False
+
+model=Sequential()
+model.add(Conv2D(32, (7, 7), padding='same', input_shape=(48,48,3)))
+model.add(MaxPooling2D(pool_size=(3, 3)))
+#dense
+model.add(Dense(6, activation='relu'))
+model.add(Conv2D(64, (1, 1), padding='same'))
+model.add(AveragePooling2D(pool_size=(2, 2)))
+#dense
+model.add(Dense(12, activation='relu'))
+model.add(Conv2D(64, (1, 1), padding='same'))
+model.add(AveragePooling2D(pool_size=(2, 2)))
+#dense
+model.add(Dense(32, activation='relu'))
+model.add(AveragePooling2D(pool_size=(2, 2)))
+model.add(Dense(32, activation='relu'))
+model.add(GlobalAveragePooling2D())
+#full connected layers
+model.add(Dense(64, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(7, activation='softmax'))
+
+model.summary()
+
+plot_model(model, to_file='densenet.png', show_shapes=True,show_layer_names=True)
+Image(filename='densenet.png') 
+
+def f1_score(y_true, y_pred): #taken from old keras source code
+    true_positives = keras.backend.sum(keras.backend.round(keras.backend.clip(y_true * y_pred, 0, 1)))
+    possible_positives = keras.backend.sum(keras.backend.round(keras.backend.clip(y_true, 0, 1)))
+    predicted_positives = keras.backend.sum(keras.backend.round(keras.backend.clip(y_pred, 0, 1)))
+    precision = true_positives / (predicted_positives + keras.backend.epsilon())
+    recall = true_positives / (possible_positives + keras.backend.epsilon())
+    f1_val = 2*(precision*recall)/(precision+recall+keras.backend.epsilon())
+    return f1_val
+
+METRICS = [
+      BinaryAccuracy(name='accuracy'),
+      Precision(name='precision'),
+      Recall(name='recall'),  
+      AUC(name='auc'),
+        f1_score,
+]
+
+lrd = ReduceLROnPlateau(monitor = 'val_loss',patience = 20,verbose = 1,factor = 0.50, min_lr = 1e-10)
+
+mcp = ModelCheckpoint('model_fer_densenet169.h5')
+
+es = EarlyStopping(verbose=1, patience=20)
+
+model.compile(optimizer='Adam', loss='categorical_crossentropy',metrics=METRICS)
+
+history=model.fit(train_dataset,validation_data=valid_dataset,epochs = 50,verbose = 1)
+
+def Train_Val_Plot(acc,val_acc,loss,val_loss,auc,val_auc,precision,val_precision,f1,val_f1):
+    
+    fig, (ax1, ax2,ax3,ax4,ax5) = plt.subplots(1,5, figsize= (20,5))
+    fig.suptitle(" MODEL'S METRICS VISUALIZATION ")
+
+    ax1.plot(range(1, len(acc) + 1), acc)
+    ax1.plot(range(1, len(val_acc) + 1), val_acc)
+    ax1.set_title('History of Accuracy')
+    ax1.set_xlabel('Epochs')
+    ax1.set_ylabel('Accuracy')
+    ax1.legend(['training', 'validation'])
+
+
+    ax2.plot(range(1, len(loss) + 1), loss)
+    ax2.plot(range(1, len(val_loss) + 1), val_loss)
+    ax2.set_title('History of Loss')
+    ax2.set_xlabel('Epochs')
+    ax2.set_ylabel('Loss')
+    ax2.legend(['training', 'validation'])
+    
+    ax3.plot(range(1, len(auc) + 1), auc)
+    ax3.plot(range(1, len(val_auc) + 1), val_auc)
+    ax3.set_title('History of AUC')
+    ax3.set_xlabel('Epochs')
+    ax3.set_ylabel('AUC')
+    ax3.legend(['training', 'validation'])
+    
+    ax4.plot(range(1, len(precision) + 1), precision)
+    ax4.plot(range(1, len(val_precision) + 1), val_precision)
+    ax4.set_title('History of Precision')
+    ax4.set_xlabel('Epochs')
+    ax4.set_ylabel('Precision')
+    ax4.legend(['training', 'validation'])
+    
+    ax5.plot(range(1, len(f1) + 1), f1)
+    ax5.plot(range(1, len(val_f1) + 1), val_f1)
+    ax5.set_title('History of F1-score')
+    ax5.set_xlabel('Epochs')
+    ax5.set_ylabel('F1 score')
+    ax5.legend(['training', 'validation'])
+
+
+    plt.show()
+
+Train_Val_Plot(history.history['accuracy'],history.history['val_accuracy'],
+               history.history['loss'],history.history['val_loss'],
+               history.history['auc'],history.history['val_auc'],
+               history.history['precision'],history.history['val_precision'],
+               history.history['f1_score'],history.history['val_f1_score']
+              )

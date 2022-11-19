fe3b32b9bbed78ae26bebc404cc55de541dbced1
luciasousa <luciamsousa00@ua.pt>
2022-11-19 14:56:30 +0000

    train models article 13

--- lucia/test_model.py
+++ lucia/test_model.py
@@ -1,5 +1,5 @@
+import os
 from tensorflow.keras.models import load_model
-import sys
 import cv2
 import dlib
 import numpy as np
@@ -26,7 +26,7 @@ def rect_to_bb(rect):
 #Draw the contours
 def draw_contour(shape):
 	convexHull = cv2.convexHull(shape)
-	cont = cv2.drawContours(frame, [convexHull], -1, (255, 0, 0), 2)
+	cont = cv2.drawContours(image, [convexHull], -1, (255, 0, 0), 2)
 	
 	return cont
 
@@ -69,11 +69,11 @@ detector = dlib.get_frontal_face_detector()
 predictor = dlib.shape_predictor('shape_predictor_68_face_landmarks.dat')
 
 #FOR VIDEO
-
+'''
 video_capture = cv2.VideoCapture(0)
-model=load_model('model.h5')
+model=load_model('../../model_lucia.h5')
 model.summary()
-filtered_face = np.zeros((1, 32, 32, 1))
+filtered_face = np.zeros((1, 48, 48, 1))
 while True:
     ret, frame = video_capture.read()
     if ret:
@@ -94,7 +94,7 @@ while True:
                 (x, y, w, h) = rect_to_bb(d)
                 bb.append((x, y, w, h))
                 cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
-            for n in range(0, 68):
+            for n in range(len(shape)):
                 x = shape[i][n][0]
                 y = shape[i][n][1]
                 cv2.circle(frame, (x, y), 4, (255, 0, 0), -1)
@@ -133,26 +133,22 @@ video_capture.release()
 cv2.destroyAllWindows()
 
 '''
-#FOR IMAGES
-
-#model = load_model('model.h5')
-#load model from article18 forlder denseNet169_JAFFE.h5
-model = load_model('denseNet169_JAFFE.h5')
-model.summary()
-
-for f in sys.argv[1:]:
-    print("Processing file: {}".format(f))
-    img = dlib.load_rgb_image(f)
-    image = cv2.imread(f)
+model = load_model('../../model_lucia.h5')
+images = []
+for filename in os.listdir('images'):
+    images.append('images/'+filename)
+        
+for img in images:
+    image = cv2.imread(img)
     gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     shape = []
     bb = []
     # The 1 in the second argument indicates that we should upsample the image
     # 1 time.  This will make everything bigger and allow us to detect more
     # faces.
-    dets = detector(img, 1)
+    dets = detector(image, 1)
     print("Number of faces detected: {}".format(len(dets)))
-    _, scores, idx = detector.run(img, 1, -1)
+    _, scores, idx = detector.run(image, 1, -1)
     for i, d in enumerate(dets):
         print("Detection {}: Left: {} Top: {} Right: {} Bottom: {}".format(i, d.left(), d.top(), d.right(), d.bottom()))
         if d is not None and d.top() >= 0 and d.right() <= gray_image.shape[1] and d.bottom() <= gray_image.shape[0] and d.left() >= 0:
@@ -175,28 +171,23 @@ for f in sys.argv[1:]:
         #SECOND - process the image, rotate, crop, increase contrast, remove noise
         for i in range(0,len(shape)):
             face = []
-            processed_image, landmarks = rotate(gray_image, shape[i])
-            processed_image = crop_face(processed_image, landmarks)
-            image = draw_contour(landmarks)
-            # Gaussian blurring is highly effective in removing Gaussian noise from an image.
-            processed_image = cv2.GaussianBlur(processed_image,(3,3),0)
-            # Histogram equalization is a method in image processing of contrast adjustment using the image's histogram.
-            processed_image = cv2.equalizeHist(processed_image)
-            resize_image = cv2.resize(processed_image, (48,48))
-            face.append(resize_image)
+            rotated_img, landmarks = rotate(gray_image, shape[i])
+            cropped_face = crop_face(rotated_img, landmarks)
+            eq_face = cv2.equalizeHist(cropped_face)
+            filtered_face = cv2.GaussianBlur(eq_face, (5, 5), 0)
+            resized_face = cv2.resize(filtered_face, (48, 48), interpolation = cv2.INTER_AREA)
+            face.append(resized_face)
             face = np.array(face)
             face = face.reshape(face.shape[0], face.shape[1], face.shape[2], 1)
             face = face.astype('float32') / 255
-            #THIRD - infer emotion
             prediction = model.predict(face)
             emotion = get_emotion(np.argmax(prediction))
-            x = bb[i][0]-10
+            x = bb
             y = bb[i][1]+bb[i][2]+20
-            cv2.putText(image, emotion, (x, y), cv2.FONT_HERSHEY_COMPLEX_SMALL, 1, (0, 0, 255), thickness=1, lineType=2)
-
-    cv2.imshow("image", image)
-    cv2.imshow("processed_image", processed_image)
+            #cv2.putText(image, emotion, (x, y), cv2.FONT_HERSHEY_COMPLEX_SMALL, 1, (0, 0, 255), thickness=1, lineType=2)
+            print(emotion)
+    cv2.imshow('image', image)
+    cv2.imshow('filtered_face', filtered_face)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
-'''

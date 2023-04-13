3cb5053f522eb7e1b944448d51104b44114c9927
luciasousa <luciamsousa00@ua.pt>
2023-04-13 10:40:43 +0100

    resnet model with k-fold cross validation

--- tese/resnet_kfold_tune.py
+++ tese/resnet_kfold_tune.py
@@ -2,11 +2,10 @@ import tensorflow as tf
 from keras.preprocessing.image import ImageDataGenerator
 from keras.models import Sequential, Model
 from keras.layers import Input, Dense, Flatten, Dropout, GlobalAveragePooling2D
-from keras.optimizers import Adam
 from keras.applications.resnet import ResNet50, preprocess_input
 import numpy as np
 import matplotlib.pyplot as plt
-from keras.optimizers import SGD
+from tensorflow.keras.optimizers import SGD
 from sklearn.metrics import classification_report, confusion_matrix
 from sklearn.model_selection import KFold, train_test_split
 import pandas as pd
@@ -18,11 +17,11 @@ import cv2
 
 
 num_classes = 2
-path_dataset = "../../../main_dataset/main_dataset/"
+path_dataset = "../../main_dataset/"
 
-train_dataset = "../../../main_dataset/main_dataset/train"
-test_dataset = "../../../main_dataset/main_dataset/test"
-val_dataset = "../../../main_dataset/main_dataset/val"
+train_dataset = "../../main_dataset/train"
+test_dataset = "../../main_dataset/test"
+val_dataset = "../../main_dataset/val"
 
 train_datagen = ImageDataGenerator(
     rescale=1./255,
@@ -63,13 +62,25 @@ test_generator = test_datagen.flow_from_directory(
     shuffle=False
 )
 
-#higher weight for the class with less samples (neutral class)
-class_weights = {0: 1., 1: 1.}
+neutral = 6821
+notneutral = 30209
+total = neutral + notneutral
 
-# Load pre-trained VGG16 model without the top layers
+# Scaling by total/2 helps keep the loss to a similar magnitude.
+# The sum of the weights of all examples stays the same.
+weight_for_0 = (1 / neutral) * (total / 2.0)
+weight_for_1 = (1 / notneutral) * (total / 2.0)
+
+print('Weight for class 0: {:.2f}'.format(weight_for_0))
+print('Weight for class 1: {:.2f}'.format(weight_for_1))
+
+#higher weight for the class with less samples (neutral class)
+class_weights = {0: weight_for_0, 1: weight_for_1}
+
+# Load pre-trained resnet model without the top layers
 base_model = ResNet50(weights='imagenet', include_top=False)
 
-# Freeze layers up to the last convolutional block of VGG16
+# Freeze layers up to the last convolutional block of resnet
 for layer in base_model.layers[:-22]:
     layer.trainable = False
 
@@ -78,7 +89,7 @@ x = GlobalAveragePooling2D()(base_model.output)
 x = Dense(128, activation='relu')(x)
 output = Dense(num_classes, activation='softmax')(x)
 # Create new model with the VGG16 base and the top layers
-model = Model(inputs=base_model.input, outputs=x)
+model = Model(inputs=base_model.input, outputs=output)
 
 # Compile the model with a low learning rate
 opt = SGD(lr=0.001, momentum=0.9)
@@ -166,10 +177,10 @@ opt = SGD(lr=0.0001, momentum=0.9)
 model.compile(optimizer=opt, loss='categorical_crossentropy', metrics=['accuracy'])
 
 # Fine-tune the model on your own dataset
-history = model.fit(train_generator, epochs=10, validation_data=val_generator, class_weight=class_weights)
+history = model.fit(train_generator, epochs=50, validation_data=val_generator, class_weight=class_weights)
 
 #save the model
-model.save('./resnet.h5')
+model.save('../../resnet.h5')
 
 #plot the accuracy and loss
 plt.plot(history.history['accuracy'])
@@ -205,3 +216,4 @@ print('Classification Report')
 print(classification_report(test_generator.classes, y_pred, target_names=['neutral', 'notneutral']))
 print('Confusion Matrix')
 print(confusion_matrix(test_generator.classes, y_pred))
+
